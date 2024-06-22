9abe40f499893e9409bcae4aea4e00d8bde9cf68
irajgill <138975046+irajgill@users.noreply.github.com>
2024-06-22 20:48:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+// INTERNAL IMPORT
+import { Footer, HeroSection, Header, Card } from "../ components/index";
 
-const index = () => {
-  return <div>index</div>;
+const home = () => {
+return (
+<div className="bg-[#1A1A1A]">
+Hey
+{/* <Header />
+‹HeroSection />
+‹Card />
+<Footer /> */}
+</div>
+);
 };
 
-export default index;
+export default home;
