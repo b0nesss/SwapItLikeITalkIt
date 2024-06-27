dd36e4b29402541eae05c583a0051eceef78971d
b0nesss <bighnesh147@gmail.com>
2024-06-27 07:18:13 +0530

    created correct abi and changed utils

--- utils/context.js
+++ utils/context.js
@@ -1,105 +1,103 @@
 import { BigNumber, ethers } from "ethers";
-import {contract , tokenContract} from "./contract";
+import { contract, tokenContract } from "./contract";
 import { toEther, toWei } from "./utils";
 
-export async function swapEthToToken(tokenName , amount){
-    try{
-        let tx = {value: toWei(amount)};
+export async function swapEthToToken(tokenName, amount) {
+    try {
+        let tx = { value: toWei(amount) };
 
-        const  contractObj = await contract();
+        const contractObj = await contract();
         const data = contractObj.swapEthToToken(tokenName, tx);
         console.log(data);
-    }catch(e){
-        console.log(e);
-    }
-}   
-
-export async function hasValidAllownace(tokenName , owner , amount){
-    try{
-        const contractObj = await contract();
-       const address = await contractObj.getTokenAddress(tokenName);
-       const tokenContractObj = await tokenContract(address);
-       const data = await tokenContractObj.allowance(owner, "0x8C3dfF46acDc96987983bc723B7f2cAAF0653EF4" )//public address);
-       console.log(data);
-       const result = BigNumber.from(data.toStrin()).gte(BigNumber.from(toWei(amount)));
-       return result;
-       console.log(result);
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 }
 
-export async function swapTokenToEth(tokenName , amount){
-   try{
-    const contractObj = await contract();
-    const data = await contractObj.swapTokenToEth(tokenName, toWei(amount));
+export async function hasValidAllownace(tokenName, owner, amount) {
+    try {
+        const contractObj = await contract();
+        const address = await contractObj.getTokenAddress(tokenName);
+        const tokenContractObj = await tokenContract(address);
+        const data = await tokenContractObj.allowance(owner, "0x78c83fAF47d814E70F6bF3948738703C93BfEDEb")
+        console.log(data);
+        const result = BigNumber.from(data.toStrin()).gte(BigNumber.from(toWei(amount)));
+        return result;
+        console.log(result);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-    const receipt = await data.wait();
-    console.log(receipt);
-    return receipt;
-   }catch(e){
-    console.log(e);
-   }
-   }
+export async function swapTokenToEth(tokenName, amount) {
+    try {
+        const contractObj = await contract();
+        const data = await contractObj.swapTokenToEth(tokenName, toWei(amount));
+
+        const receipt = await data.wait();
+        console.log(receipt);
+        return receipt;
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 
-   export async function swapTokenToToken(srcToken , destToken , amount){
-    try{
+export async function swapTokenToToken(srcToken, destToken, amount) {
+    try {
         const contractObj = await contract();
         const data = await contractObj.swapTokenToToken(srcToken, destToken, toWei(amount));
         const receipt = await data.wait();
         console.log(receipt);
         return receipt;
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
-   }
+}
 
 
-   export async function getTokenBalance(tokenName , address){
+export async function getTokenBalance(tokenName, address) {
     const contractObj = await contract();
-    const balance = await contractObj.getTokenBalance(tokenName, address);
+    const balance = await contractObj.getBalance(tokenName, address);
     return balance;
+}
 
-   }
-
-   export async function getTokenAddress(tokenName){
+export async function getTokenAddress(tokenName) {
     const contractObj = await contract();
     const address = await contractObj.getTokenAddress(tokenName);
     return address;
-   }
+}
 
-   export async function increaseAllowance(tokenName , amount){
+export async function increaseAllowance(tokenName, amount) {
     const contractObj = await contract();
     const address = await contractObj.getTokenAddress(tokenName);
     const tokenContractObj = await tokenContract(address);
-    const data = await tokenContractObj.increaseAllowance("0x8C3dfF46acDc96987983bc723B7f2cAAF0653EF4", toWei(amount));
+    const data = await tokenContractObj.increaseAllowance("0x78c83fAF47d814E70F6bF3948738703C93BfEDEb", toWei(amount));
     const receipt = await data.wait();
     console.log(receipt);
     return receipt;
-   }
+}
 
-   export async function getAllHistory(){
-    try{
+export async function getAllHistory() {
+    try {
         const contractObj = await contract();
         const getAllHistory = await contractObj.getAllHistory();
 
         const historyTransaction = getAllHistory.map((history, i) => ({
-            historyId : history.historyId.toNumber(),
-            tokenA : history.tokenA,
-            tokenB : history.tokenB,
+            historyId: history.historyId.toNumber(),
+            tokenA: history.tokenA,
+            tokenB: history.tokenB,
             inputValue: toEther(history?.inputValue),
             outputValue: toEther(history?.outputValue),
             userAddress: history.userAddress,
 
         }))
         return historyTransaction;
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
-    
-    
-   }
 
 
-   
+}
+
+
--- utils/contract.js
+++ utils/contract.js
@@ -1,7 +1,7 @@
 import {ethers} from 'ethers'
 
-import CustomDexABI from "./CustomDex.json"
-import CustomTokenABI from "./CustomToken.json"
+import CustomDexABI from "./dexaabi.json"
+import CustomTokenABI from "./tokenabi.json"
 
 
 
@@ -11,7 +11,7 @@ export const tokenContract = async (address) => {
 
     if(ethereum){
         const signer = provider.getSigner();
-        const contractReader = new ethers.Contract(address, CustomTokenABI.abi, signer);
+        const contractReader = new ethers.Contract(address, CustomTokenABI, signer);
         return contractReader;
     }
    
@@ -21,10 +21,14 @@ export const tokenContract = async (address) => {
 export const contract = async () =>{
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const {ethereum} = window;
-
+    console.log(`${CustomDexABI.abi} is ABI`)
     if(ethereum){
         const signer = provider.getSigner();
-        const contractReader = new ethers.Contract("0x8C3dfF46acDc96987983bc723B7f2cAAF0653EF4", CustomDexABI.abi, signer);
+        const contractReader = new ethers.Contract("0x78c83fAF47d814E70F6bF3948738703C93BfEDEb", CustomDexABI, signer);
         return contractReader;
     }
 }
+
+//0x78c83fAF47d814E70F6bF3948738703C93BfEDEb - new address
+
+//0x8C3dfF46acDc96987983bc723B7f2cAAF0653EF4 - old address
